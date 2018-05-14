61711e98600bf67f3ee9aecb2a3ad29a198c5f44
chihirons <chihi60323@icloud.com>
2018-05-14 11:29:10 +0900

    Initial commit

---
+++ app/src/main/java/filedownloader/abs/co/jp/tetlis_test1/Tetris.java
@@ -0,0 +1,12 @@
+package filedownloader.abs.co.jp.tetlis_test1;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Tetris extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tetris);
+    }
+}
